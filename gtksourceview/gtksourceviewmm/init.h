275a5ba41aa06e2a4d21788616f913b7e2fef1e1
Dodji Seketeli <dodji@src.gnome.org>
2006-11-11 15:48:49 +0000

    Sat, 11 Nov 2006 16:20:21 +0100 Dodji Seketeli
    	* gtksourceview/src/*.hg: update the API docs. All the classes are now
    	  basically documented.
    	* docs/reference/Makefile.am: make install now installs the docs
    	  in docdir/reference/html. APIDOCs are shipped in the tarball now.
    
    
    svn path=/trunk/; revision=30

--- gtksourceview/gtksourceviewmm/init.h
+++ gtksourceview/gtksourceviewmm/init.h
@@ -22,6 +22,8 @@
  * License along with this library; if not, write to the Free
  * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
+/// \file
+/// \brief the library initialization routines
 namespace gtksourceview
 {
     void init () ;
